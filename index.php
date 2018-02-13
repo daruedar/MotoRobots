f86e92c77cefad5251ba5ff416a3d1eb319c0c4d
Diego Rueda <daruedar@hotmail.com>
2018-02-13 18:41:41 -0430

    Definiendo Funciones

--- index.php
+++ index.php
@@ -2,3 +2,6 @@ contenido principal de la pagina
 En esta pagina  deben aparecer  es el index
 de mi sitio, acá van los baner y la informacion 
 de la empresa
+Es importante que  todos  los ver mas de las  imagenes
+apunten  hacia el catalogo general de repuestos-de-motos
+para que el usuario desde ahi se ubuque  con los fabricantes y repuestos que busca
