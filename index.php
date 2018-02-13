2ef2bd253d7c0e2a1532288cf8ed17a368faf743
Diego Rueda <daruedar@hotmail.com>
2018-02-13 19:00:26 -0430

    Definiendo Funcionalidad

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ de la empresa
 Es importante que  todos  los ver mas de las  imagenes
 apunten  hacia el catalogo general de repuestos-de-motos
 para que el usuario desde ahi se ubuque  con los fabricantes y repuestos que busca
+
+El menu de navegacion izquierdo por optimizacion pARA INDECXACINO CON GOOGLE
+lo podemos decir con los  nombres  de los  repuestos  tal cual  se  hizo al principio
+y de la misma  manera que figuran en el catalogo impreso
--- repuestos-de-motos/index.php
+++ repuestos-de-motos/index.php
@@ -2,3 +2,6 @@ en esta pagina van los  logos
 de los fabricantes en la  parte central, es decir yamaha
 y en la parte izquierda, los mismos fabricantes,desplegando sus modelos
 para un acceso mas rapido
+Aunque  tambien puede ser  que a la derecha  tengamos 
+el menu pro nombre de repuestos para  que el usuario tenga 
+las  dos  alterativas de busqueda en  esta pagina
