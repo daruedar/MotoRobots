479a2c93190b61a8451540703c5ebb6bcfcf65ca
Diego Rueda <daruedar@hotmail.com>
2018-02-13 18:19:09 -0430

    Definiendo  contenidos

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 contenido principal de la pagina
+En esta pagina  deben aparecer  los 6 o nueve 
+Fabricantes de motocicletas en la parte central
+Y en la parte Derecha   a menera  de opcion, el tipo de Repuesto 
+que Buscan
