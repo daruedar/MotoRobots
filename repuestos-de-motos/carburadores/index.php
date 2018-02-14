55d508a6837a633b10749aa578cea48289755f6f
Diego Rueda <daruedar@hotmail.com>
2018-02-13 20:01:11 -0430

    Definiendo el flujo de Informacion

---
+++ repuestos-de-motos/carburadores/index.php
@@ -0,0 +1,3 @@
+esta es la pagian donde en la parte central se muestran todos
+los carburadores disponibles, sin importan fabricante o modelo.
+En el menu izquierdo se muestra todos los otros  repuestos disponibles
